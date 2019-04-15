5caf7ed59aa88a4e6ac7bdfba85bb3ee818a3e21
Michael <michael.abr@hotmail.com>
2019-04-14 21:17:01 -0300

    cadastro no banco OK

--- app/src/main/java/com/example/energymonitor/CadastroActivity.java
+++ app/src/main/java/com/example/energymonitor/CadastroActivity.java
@@ -15,25 +15,36 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CadastroActivity extends AppCompatActivity {
 
-    private EditText inputEmail, inputPassword;     //hit option + enter if you on mac , for windows hit ctrl + enter
+    private EditText inputEmail, inputPassword, inputName, inputDataNascimento, inputTelefone;     //hit option + enter if you on mac , for windows hit ctrl + enter
     private Button btnSignIn, btnSignUp, btnResetPassword;
     private ProgressBar progressBar;
     private FirebaseAuth auth;
+    private DatabaseReference databaseReference, databaseMedidor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
-        //Get Firebase auth instance
+
+        //Inicializando firebase auth objeto
         auth = FirebaseAuth.getInstance();
+        //FirebaseUser user = auth.getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
 
         btnSignIn = (Button) findViewById(R.id.sign_in_button);
         btnSignUp = (Button) findViewById(R.id.sign_up_button);
         inputEmail = (EditText) findViewById(R.id.email);
         inputPassword = (EditText) findViewById(R.id.password);
+        inputName = findViewById(R.id.name);
+        inputDataNascimento = findViewById(R.id.datanascimento);
+        inputTelefone = findViewById(R.id.telefone);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         btnResetPassword = (Button) findViewById(R.id.btn_reset_password);
 
@@ -59,33 +70,53 @@ public class CadastroActivity extends AppCompatActivity {
                 String password = inputPassword.getText().toString().trim();
 
                 if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Coloque o email", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Coloque a senha!", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 if (password.length() < 6) {
-                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Senha muito curta, no mínimo 6 caracteres!", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 progressBar.setVisibility(View.VISIBLE);
+
                 //create user
                 auth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(CadastroActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                Toast.makeText(CadastroActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+
+                                if(auth.getCurrentUser() != null) {
+                                    String name = inputName.getText().toString().trim();
+                                    String datanascimento = inputDataNascimento.getText().toString().trim();
+                                    String telefone = inputTelefone.getText().toString().trim();
+
+                                    UserInformation userInformation = new UserInformation(name, datanascimento, telefone);
+
+                                    FirebaseUser user = auth.getCurrentUser();
+
+                                    databaseReference.child(user.getUid()).setValue(userInformation);
+
+                                    databaseMedidor = FirebaseDatabase.getInstance().getReference(user.getUid());
+
+                                    databaseMedidor.child("Leitura").setValue(1);
+
+
+                                    Toast.makeText(CadastroActivity.this, "Usuário criado com sucesso" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                }
+                                //Toast.makeText(CadastroActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
                                 progressBar.setVisibility(View.GONE);
                                 // If sign in fails, display a message to the user. If sign in succeeds
                                 // the auth state listener will be notified and logic to handle the
                                 // signed in user can be handled in the listener.
                                 if (!task.isSuccessful()) {
-                                    Toast.makeText(CadastroActivity.this, "Authentication failed." + task.getException(),
+                                    Toast.makeText(CadastroActivity.this, "Falha na autenticação " + task.getException(),
                                             Toast.LENGTH_SHORT).show();
                                 } else {
                                     startActivity(new Intent(CadastroActivity.this, MeuPerfilActivity.class));
---
+++ app/src/main/java/com/example/energymonitor/UserInformation.java
@@ -0,0 +1,14 @@
+package com.example.energymonitor;
+
+public class UserInformation {
+
+    public String name;
+    public String datanascimento;
+    public String telefone;
+
+    public UserInformation(String name, String datanascimento, String telefone){
+        this.name = name;
+        this.datanascimento = datanascimento;
+        this.telefone = telefone;
+    }
+}
